ad6991c4c248a445d37fa0e2f0f0b3d424f21606
Ajay Priyadarshi <ajbj444@gmail.com>
2024-01-05 17:48:04 +0530

    Database complete

--- src/models/Contents.js
+++ src/models/Contents.js
@@ -2,7 +2,6 @@
 
 import mongoose from 'mongoose';
 
-// Define the content schema
 const contentSchema = new mongoose.Schema({
   User_ID: { type: mongoose.Schema.Types.ObjectId, ref: 'User_Data_tbl', required: true },
   Content_Type: { type: String, required: true },
@@ -11,8 +10,6 @@ const contentSchema = new mongoose.Schema({
   Upload_TimeStamp: { type: Date, default: Date.now },
 });
 
-// Create the Content model
 const ContentModel = mongoose.model('Content_tbl', contentSchema, 'Content_tbl');
 
-// Export the Content model as a named export
 export { ContentModel };

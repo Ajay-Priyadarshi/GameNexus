f6647adc60958f59c0b4df008d9d6dbbaac36fef
Ajay Priyadarshi <ajbj444@gmail.com>
2024-03-01 19:50:00 +0530

    comment complete

--- src/controllers/commentController.js
+++ src/controllers/commentController.js
@@ -1,12 +1,45 @@
 import { ContentModel as Content } from '../models/Contents.js';
 import { CommentModel as Comment } from '../models/Comment.js';
+import { UserModel as User } from '../models/User.js';
 
 export const getComments = async (req, res) => {
+    const userId = req.session.userId;
     const postId = req.params.postId;
-    const post = await Content.findById(postId).populate('User_ID');
+
     try {
-        const comments = await Comment.find({ Content_ID: postId }).populate('User_ID');
-        res.render('comment', { post, comments });
+        const user = await User.findById(userId);
+        const post = await Content.findById(postId)
+            .populate('User_ID')
+            .exec();
+        const comments = await Comment.find({ Content_ID: postId })
+            .populate('User_ID', 'username _id')
+            .exec();
+
+        res.render('comment', { user, post, comments });
+        
+    } catch (error) {
+        console.error("error");
+        res.status(500).send('Internal Server Error');
+    }
+}
+
+export const createComment = async (req, res) => {
+    const userId = req.session.userId;
+    const { Content_ID, Comment_Description } = req.body;
+
+    try {
+        const newComment = new Comment({
+            Content_ID: Content_ID,
+            User_ID: userId,
+            Comment_Description: Comment_Description
+        });
+        await newComment.save();
+        return res.status(200).send(`
+        <script>
+          alert('Comment added.');
+          window.location.href = '/comment/${Content_ID}';
+        </script>
+      `);
     } catch (error) {
         console.error(error);
         res.status(500).send('Internal Server Error');
--- src/routes/commentRoutes.js
+++ src/routes/commentRoutes.js
@@ -1,9 +1,10 @@
 // commentRoutes.js
 import express from "express";
-import { getComments } from "../controllers/commentController.js";
+import { getComments, createComment } from "../controllers/commentController.js";
 
 const router = express.Router();
 
 router.get('/:postId', getComments);
+router.post('/create', createComment);
 
 export default router;
